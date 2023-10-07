c48913d83646c7482145d6cf2393070b05cb308d
r=cos(4θ) <olvadis2004@gmail.com>
2023-10-07 14:09:08 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.code.parcial1_olvadishernandezledesma_oct7 {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens com.code.parcial1_olvadishernandezledesma_oct7 to javafx.fxml;
+  exports com.code.parcial1_olvadishernandezledesma_oct7;
+}
