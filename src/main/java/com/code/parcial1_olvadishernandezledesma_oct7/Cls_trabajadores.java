aff434a05cc59c43d9f5e5ae0be6827d43729127
r=cos(4θ) <olvadis2004@gmail.com>
2023-10-07 14:16:39 -0500

    Creacion de la clase Cls_trabajadores
    Se creo la clase trabajador con sus atributos y metodos

---
+++ src/main/java/com/code/parcial1_olvadishernandezledesma_oct7/Cls_trabajadores.java
@@ -0,0 +1,90 @@
+package com.code.parcial1_olvadishernandezledesma_oct7;
+
+public class Cls_trabajadores {
+  private int id;
+  private String nombres;
+  private String apellidos;
+  private String contact;
+  private String correo;
+  private String direccion;
+  private String sexo;
+  private double salario;
+
+  public Cls_trabajadores(int id, String nombres, String apellidos,
+                          String contact, String correo,
+                          String direccion, String sexo,
+                          double salario) {
+    this.id = id;
+    this.nombres = nombres;
+    this.apellidos = apellidos;
+    this.contact = contact;
+    this.correo = correo;
+    this.direccion = direccion;
+    this.sexo = sexo;
+    this.salario = salario;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNombres() {
+    return nombres;
+  }
+
+  public void setNombres(String nombres) {
+    this.nombres = nombres;
+  }
+
+  public String getApellidos() {
+    return apellidos;
+  }
+
+  public void setApellidos(String apellidos) {
+    this.apellidos = apellidos;
+  }
+
+  public String getContact() {
+    return contact;
+  }
+
+  public void setContact(String contact) {
+    this.contact = contact;
+  }
+
+  public String getCorreo() {
+    return correo;
+  }
+
+  public void setCorreo(String correo) {
+    this.correo = correo;
+  }
+
+  public String getDireccion() {
+    return direccion;
+  }
+
+  public void setDireccion(String direccion) {
+    this.direccion = direccion;
+  }
+
+  public String getSexo() {
+    return sexo;
+  }
+
+  public void setSexo(String sexo) {
+    this.sexo = sexo;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+}
